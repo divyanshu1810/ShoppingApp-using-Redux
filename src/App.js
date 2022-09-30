53d93e87cfef888154e54e6cc29b4ca4ceac6b03
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-10-01 00:00:30 +0530

    :sparkles: Authentication
    Auth using redux added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  // console.log(isLoggedIn)
   return (
     <div className="App">
-      <Auth />
-      {/* <Layout /> */}
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <Layout />}
     </div>
   );
 }
